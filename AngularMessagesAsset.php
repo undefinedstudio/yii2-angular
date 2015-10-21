7f3b3c0c1f6e3a112ebdcef94fb07b578d1f9bf1
Gabriele Della Casa Venturelli <hardsetting@gmail.com>
2015-10-21 02:43:58 +0200

    added angular-messages support

---
+++ AngularMessagesAsset.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace undefinedstudio\yii2\angular;
+
+use yii\web\AssetBundle;
+
+class AngularMessagesAsset extends AssetBundle
+{
+    public $sourcePath = '@vendor/bower/angular-messages';
+    public $js = [
+        'angular-messages.js',
+    ];
+
+    public $depends = [
+        'undefinedstudio\yii2\angular\AngularAsset',
+    ];
+}
